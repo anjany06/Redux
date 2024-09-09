a417adca2ec4bd439755caf8247a5a0f52390558
Anjany <anjany.pandey06@gmail.com>
2024-09-09 18:40:30 +0530

    Learn how to increment decrement and add using redux

--- kg-coding-redux.js
+++ kg-coding-redux.js
@@ -9,6 +9,12 @@ const reducer = (store = INTIAL_VALUE, action) =>{
   if(action.type === 'INCREMENT'){
     newStore = {counter : store.counter+1}
   }
+  else if(action.type === 'DECREMENT'){
+    newStore = {counter : store.counter-1}
+  }
+  else if(action.type === 'ADDITION'){
+    newStore = {counter : store.counter + action.payload.number}
+  }
   return newStore;
 }
 const store = redux.createStore(reducer);
@@ -20,3 +26,5 @@ const subscriber = () =>{
 store.subscribe(subscriber);
 
 store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'DECREMENT'});
+store.dispatch({type: 'ADDITION', payload :{number : 7}})
