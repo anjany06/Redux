4257fb069a2749515dd3a072db87637217017a1c
Anjany <anjany.pandey06@gmail.com>
2024-09-09 23:53:40 +0530

    Learned using redux with react

---
+++ ReactRedux/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./component/Header";
+import DisplayCounter from "./component/DisplayCounter";
+import Model from "./component/Model";
+import Controls from "./component/Controls";
+
+function App() {
+  return (
+    <>
+      <center className="px-4 py-5 my-5 text-center">
+        <Model>
+          <Header></Header>
+          <div className="col-lg-6 mx-auto">
+            <DisplayCounter></DisplayCounter>
+            <Controls></Controls>
+          </div>
+        </Model>
+      </center>
+    </>
+  );
+}
+
+export default App;
---
+++ ReactRedux/src/component/DisplayCounter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DisplayCounter() {
+  return (
+    <div>
+      <p className="lead mb-4">Counter Current Value :</p>
+    </div>
+  );
+}
+
+export default DisplayCounter;
