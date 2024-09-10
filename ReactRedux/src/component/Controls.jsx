dbd385f3116ff88b7fac6a47e1252d9b769955c8
Anjany <anjany.pandey06@gmail.com>
2024-09-10 22:02:15 +0530

    created a simple app that increment or decrement counter using react with redux

--- ReactRedux/src/component/Controls.jsx
+++ ReactRedux/src/component/Controls.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 
 function Controls() {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const handleDecrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
   return (
     <>
       <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <button type="button" class="btn btn-primary">
+        <button type="button" class="btn btn-primary" onClick={handleIncrement}>
           +1
         </button>
-        <button type="button" class="btn btn-success">
+        <button type="button" class="btn btn-success" onClick={handleDecrement}>
           -1
         </button>
         {/* <button type="button" class="btn btn-danger">
--- ReactRedux/src/store/index.js
+++ ReactRedux/src/store/index.js
@@ -4,6 +4,11 @@ const INITIAL_VALUE = {
 }
 
 const counterReducer = (store = INITIAL_VALUE, action) => {
+ if(action.type === 'INCREMENT'){
+  return {counter : store.counter + 1};
+ }else if (action.type === 'DECREMENT'){
+  return {counter : store.counter - 1};
+ }
   return store;
 }
 const counterStore = createStore(counterReducer);
