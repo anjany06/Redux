a61f359fcfd6f3c901479093a4bd4239328e47b6
Anjany <anjany.pandey06@gmail.com>
2024-09-12 21:21:22 +0530

    Added a input box where type a number and add or subtract that number into the counter

--- ReactRedux/src/component/Controls.jsx
+++ ReactRedux/src/component/Controls.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 function Controls() {
   const dispatch = useDispatch();
+  const inputElement = useRef();
 
   const handleIncrement = () => {
     dispatch({ type: "INCREMENT" });
   };
   const handleDecrement = () => {
     dispatch({ type: "DECREMENT" });
+  };
+  const handleAdd = () => {
+    dispatch({
+      type: "ADD",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
+  };
+  const handleSubtract = () => {
+    dispatch({
+      type: "SUBTRACT",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
   };
   return (
     <>
@@ -34,6 +53,24 @@ function Controls() {
         <button type="button" class="btn btn-dark">
           Dark
         </button> */}
+      </div>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+        <input
+          type="text"
+          placeholder="Enter Number"
+          className="number-input"
+          ref={inputElement}
+        />
+        <button type="button" className="btn btn-info" onClick={handleAdd}>
+          Add
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={handleSubtract}
+        >
+          Subtract
+        </button>
       </div>
     </>
   );
